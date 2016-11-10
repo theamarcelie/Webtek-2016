d1893c160bb27252959429efaef4f8c868f8e255
Peder Holm <pederholmdesign@gmail.com>
2016-11-10 16:41:06 +0100

    Update onmouseover.js

--- js/onmouseover.js
+++ js/onmouseover.js
@@ -1,11 +1,11 @@
 function imageEnlarge() {
- document.getElementById('1').style.height="100%";
- document.getElementById('1').style.width="100%";
+ //document.getElementById('1').style.height="20%";
+ document.getElementById('1').style.width="20%";
  document.getElementById('1').style.position='absolute';
  }
 function imageReset() {
 
- document.getElementById('1').style.height="80%";
- document.getElementById('1').style.width="80%";
+ //document.getElementById('1').style.height="80%";
+ document.getElementById('1').style.width="0%";
  document.getElementById('1').style.position='absolute';
  }
