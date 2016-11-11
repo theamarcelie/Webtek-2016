5251284997f09f744a276e8bc3b23937bd6e025b
Peder Holm <pederholmdesign@gmail.com>
2016-11-11 11:17:07 +0100

    Update mailer.php
    La til kommentarer og byttet ut dummy epost med egen epost

--- js/mailer.php
+++ js/mailer.php
@@ -1,52 +1,48 @@
 <?php
-    // My modifications to mailer script from:
-    // http://blog.teamtreehouse.com/create-ajax-contact-form
-    // Added input sanitizing to prevent injection
-
-    // Only process POST reqeusts.
+ 
+    // Prosesserer POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
+        // Tar inn formfeltet og fjerner whitespace
         $name = strip_tags(trim($_POST["name"]));
 				$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $message = trim($_POST["message"]);
 
-        // Check that data was sent to the mailer.
+        // Sjekker om data ble sendt til maileren
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
+            // Responderer ved feilkode 400 (bad request) og avslutter
             http_response_code(400);
             echo "Oops! There was a problem with your submission. Please complete the form and try again.";
             exit;
         }
 
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "hello@example.com";
+        // Setter mottager epost 
+        $recipient = "peder.holm.design@gmail.com";
 
-        // Set the email subject.
+        // Setter epost subjektet
         $subject = "New contact from $name";
 
-        // Build the email content.
+        // Bygger epost innholdet
         $email_content = "Name: $name\n";
         $email_content .= "Email: $email\n\n";
         $email_content .= "Message:\n$message\n";
 
-        // Build the email headers.
+        // Bygger epost header
         $email_headers = "From: $name <$email>";
 
-        // Send the email.
+        // Sender epost
         if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
+            // Setter 200 som OK respons feilkode
             http_response_code(200);
             echo "Thank You! Your message has been sent.";
         } else {
-            // Set a 500 (internal server error) response code.
+            // Setter 500 som intern server respons feilkode 
             http_response_code(500);
             echo "Oops! Something went wrong and we couldn't send your message.";
         }
 
     } else {
-        // Not a POST request, set a 403 (forbidden) response code.
+        // Ikke POST forespørsel, setter 403 (forbidden) respons feilkode 
         http_response_code(403);
         echo "There was a problem with your submission, please try again.";
     }
