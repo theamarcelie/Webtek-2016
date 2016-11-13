a46414c32d263a20c888ea166c51ea538eba1af4
Peder Holm <pederholmdesign@gmail.com>
2016-11-13 21:13:52 +0100

    Update faq.js

--- js/faq.js
+++ js/faq.js
@@ -1,6 +1,6 @@
     function expand_collapse(id) {
        var e = document.getElementById(id);
-       var f = document.getElementById(id+"_arrows");
+       /*var f = document.getElementById(id+"_arrows");*/
        if(e.style.display == 'none'){
           e.style.display = 'block';
        }
