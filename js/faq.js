60a1fd73cd02b006023e27defecde55d72185f99
Peder Holm <pederholmdesign@gmail.com>
2016-11-13 21:12:36 +0100

    Update faq.js

--- js/faq.js
+++ js/faq.js
@@ -3,10 +3,10 @@
        var f = document.getElementById(id+"_arrows");
        if(e.style.display == 'none'){
           e.style.display = 'block';
-          f.innerHTML = '&#9650';
+          /*f.innerHTML = '&#9650';*/
        }
        else {
           e.style.display = 'none';
-          f.innerHTML = '&#9660';
+          /*f.innerHTML = '&#9660';*/
        }
     }
